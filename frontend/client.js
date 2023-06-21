d79c62b70f3ec5c0a7d609f45ad380d03c83af12
Kunal Vibhute <42132165+kunal98v@users.noreply.github.com>
2023-06-22 01:34:07 +0530

    Add files via upload

---
+++ frontend/client.js
@@ -0,0 +1,87 @@
+
+const socket = io()
+
+let btn = document.querySelector(".btn");
+let inputBox = document.querySelector(".inputBox");
+let chatBox = document.querySelector(".chatBox");
+let getName = document.querySelector("#name");
+
+var audio = new Audio('ting.mp3')
+
+let namee;
+do{
+    namee = prompt("enter your name");
+
+}while(!namee)
+
+getName.innerText = `Welcome ${namee}`;
+socket.emit('new-user-joined',namee);
+
+
+function append(name,msg,position){
+
+    if(position == "Right"){
+        audio.play()
+
+    }
+    let div = document.createElement('div');
+
+    div.classList.add('chat')
+    div.classList.add(position)
+    console.log(name,msg)
+    div.innerHTML = `<b>${name}</b> : ${msg}`;
+
+    var time = new Date();
+    var getTime = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+    let span = document.createElement('span')
+    span.classList.add('time')
+    span.innerText = getTime;
+    div.appendChild(span);
+    
+    if(msg){
+        chatBox.appendChild(div);
+    }
+    inputBox.value = "";
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+
+socket.on('user-joined',(name) =>{
+    console.log(name+'joined');
+    let div = document.createElement('div')
+    div.innerText = `${name} joined`
+    div.classList.add('center')
+    chatBox.appendChild(div)
+    chatBox.scrollTop = chatBox.scrollHeight;
+
+
+});
+
+
+socket.on('receive',data=>{
+    append(data.name,data.message,"Right");
+    
+
+});
+
+socket.on('left',name=>{
+    let div = document.createElement('div')
+    div.innerText = `${name} left`
+    div.classList.add('center')
+    chatBox.appendChild(div)
+    chatBox.scrollTop = chatBox.scrollHeight;
+})
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    socket.emit('send',inputBox.value)
+    append("You",inputBox.value,"Left")  
+});
+inputBox.addEventListener('keydown',(e)=>{
+    if(e.key === "Enter"){
+        e.preventDefault();
+        socket.emit('send',inputBox.value)
+        append("You",inputBox.value,"Left")  
+    }
+    
+});
